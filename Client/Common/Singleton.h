7e365e0064921f0f599bea25c6f8dece76d18dca
PorkShoulder <guswns1972@naver.com>
2026-04-15 20:44:23 +0900

    초기 커밋.

---
+++ Client/Common/Singleton.h
@@ -0,0 +1,25 @@
+#pragma once
+
+template<typename T>
+class Singleton
+{
+protected:
+	Singleton() = default;
+	~Singleton() = default; 
+	Singleton(const Singleton&) = delete;
+	Singleton(Singleton&&) = delete;
+	Singleton operator=(const Singleton&) = delete;
+	Singleton operator=(Singleton&&) = delete;
+public:
+	static T& Instance() 
+	{
+		static T Inst;
+		return Inst;
+	}
+public:
+	// 가상함수인 이유 : 싱글톤을 상속받을 자식에서 내부 동작이 달라질 것이기 때문에.
+	virtual void Init() = 0;		// = 0 : 순수가상함수라는 표시
+	virtual void Destroy() = 0;
+
+};
+
---
+++ Client/Core/GameEngine.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Client.h"
+#include "framework.h"
+#include "../Common/Singleton.h"
+
+
+
+class GameEngine : public Singleton<GameEngine>
+{
+
+
+
+
+
+
+
+
+};
+
